cc4bb5b56e1cc18b633831b58f152e3604e90774
Loren Wilks <Loren.wilks@gmail.com>
2022-07-19 10:08:38 -0600

    commit tree 717199dcce458fee16ef9967a8decbc8fecaacc7

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Loren";
+const city = "Parker";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const image = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F24.media.tumblr.com%2Fd0e9137219b78062b0441ee74ca47a8f%2Ftumblr_mjv8falGbU1s8o1r4o1_1280.gif&f=1&nofb=1";
 
 export { name, city, image };
